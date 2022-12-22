5346d68ef1b5c8d4c5ccc336cc2a5cf5fd760d73
Nino Maruszewski <nino.maruszewski@gmail.com>
2022-12-22 10:21:44 -0600

    docs: add comments

--- include/mpu.hpp
+++ include/mpu.hpp
@@ -32,16 +32,60 @@
 
 #include <MPU6050_6Axis_MotionApps612.h>
 
+/**
+ * @brief Set to true if the MPU6050 initialized correctly.
+ */
 extern bool dmp_ready;
 
+/**
+ * @brief Setup and initialize the MPU6050 and its DMP.
+ *
+ * 1. Starts the I2C bus
+ * 2. Initializes the MPU6050
+ * 3. Initializes the DMP.
+ * 4. Calibrates the DMP.
+ * 5. Attaches an ISR for data ready.
+ */
 void mpu_setup();
 
+/**
+ * @brief Poll the MPU6050 to see if data is available.
+ *
+ * If the data is available, updates the internal FIFO data buffer.
+ *
+ * @return bool If there is new DMP data available.
+ */
 bool mpu_data_available();
 
+/**
+ * @brief Get the real acceleration (without gravity).
+ *
+ * @param accel_real Container to save the accelearation to.
+ */
 void mpu_get_real_accel(VectorInt16* accel_real);
 
+/**
+ * @brief Get the real acceleration, in the world frame of reference.
+ *
+ * This rotates the acceleration to match the initial MPU6050 orientation.
+ *
+ * @param accel_world Container to save the acceration to.
+ */
 void mpu_get_world_accel(VectorInt16* accel_world);
 
+/**
+ * @brief Get the yaw/pitch/roll orientation of the MPU6050.
+ *
+ * @param ypr Container to save the YPR data to.
+ */
 void mpu_get_ypr(float ypr[3]);
 
+/**
+ * @brief Get the temperature from the MPU6050's built-in temp sensor.
+ *
+ * Theoretically good for two decimal places of accuracy, realistically you can
+ * only use one.
+ *
+ * @return float The temperature in degrees celsius.
+ */
 float mpu_get_temp();
