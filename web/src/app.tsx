f85577678830056e3815e146157348c696e7f6b2
Nino Maruszewski <nino.maruszewski@gmail.com>
2022-12-25 10:09:07 -0600

    feat: line chart

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,14 +1,21 @@
 import { useState, useEffect } from "preact/hooks";
-import preactLogo from "./assets/preact.svg";
-import { Temperature, Acceleration, YawPitchRoll } from "./components";
+import {
+  Temperature,
+  Acceleration,
+  YawPitchRoll,
+  LineChart,
+} from "./components";
 
 export function App() {
   const [connected, setConnected] = useState(false);
-  const [data, setData] = useState({
-    ypr: [0, 0, 0],
-    accel: [0, 0, 0],
-    temp: 0,
-  });
+  const [data, setData] = useState<
+    {
+      time: number;
+      ypr: number[];
+      accel: number[];
+      temp: number;
+    }[]
+  >([]);
 
   const [refresh, setRefresh] = useState(false);
 
@@ -36,13 +43,23 @@ export function App() {
     });
 
     sse.addEventListener("mpuData", (e) => {
-      setData(JSON.parse(e.data));
+      setData((data) => [
+        ...data.slice(-100, data.length),
+        { time: parseInt(e.lastEventId), ...JSON.parse(e.data) },
+      ]);
     });
 
     return () => {
       sse.close();
     };
   }, [refresh]);
+
+  const curData = data[data.length - 1] || {
+    time: 0,
+    ypr: [0, 0, 0],
+    accel: [0, 0, 0],
+    temp: 0,
+  };
 
   return (
     <>
@@ -63,9 +80,63 @@ export function App() {
         )}
       </div>
 
-      <YawPitchRoll yaw={data.ypr[0]} pitch={data.ypr[1]} roll={data.ypr[2]} />
-      <Acceleration x={data.accel[0]} y={data.accel[1]} z={data.accel[2]} />
-      <Temperature temp={data.temp} />
+      <LineChart
+        title="Test Chart"
+        xVals={[
+          Date.now(),
+          Date.now() + 1,
+          Date.now() + 2,
+          Date.now() + 3,
+          Date.now() + 4,
+        ]}
+        yVals={[[1, 2, 3, 4, 5]]}
+        series={[
+          {
+            label: "Test",
+            stroke: "red",
+          },
+        ]}
+        xAxisType="ms"
+      />
+      <LineChart
+        title="YPR"
+        xVals={data.map((e) => e.time)}
+        yVals={data.reduce<number[][]>(
+          (acc, cur) => {
+            acc[0].push(cur.ypr[0]);
+            acc[1].push(cur.ypr[1]);
+            acc[2].push(cur.ypr[2]);
+            return acc;
+          },
+          [[], [], []]
+        )}
+        series={[
+          {
+            label: "Yaw",
+            stroke: "red",
+          },
+          {
+            label: "Pitch",
+            stroke: "blue",
+          },
+          {
+            label: "Roll",
+            stroke: "green",
+          },
+        ]}
+      />
+
+      <YawPitchRoll
+        yaw={curData.ypr[0]}
+        pitch={curData.ypr[1]}
+        roll={curData.ypr[2]}
+      />
+      <Acceleration
+        x={curData.accel[0]}
+        y={curData.accel[1]}
+        z={curData.accel[2]}
+      />
+      <Temperature temp={curData.temp} />
     </>
   );
 }
--- web/src/components/index.tsx
+++ web/src/components/index.tsx
@@ -1,3 +1,4 @@
 export { Temperature } from "./temperature/Temperature";
 export { Acceleration } from "./acceleration/Acceleration";
 export { YawPitchRoll } from "./yaw-pitch-roll/YawPitchRoll";
+export { LineChart } from "./line-chart/line-chart";
