3e10d075a4766d99e40ef8eabb329673cfbdd4d8
Nino Maruszewski <nino.maruszewski@gmail.com>
2022-12-25 14:42:17 -0600

    style: add prettier

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -1,21 +1,21 @@
 /* eslint-env node */
 
 module.exports = {
-  root: true,
-  parser: "@typescript-eslint/parser",
-  extends: ["preact", "plugin:@typescript-eslint/recommended"],
-  ignorePatterns: ["dist/"],
-  rules: {
-    "@typescript-eslint/ban-ts-comment": [
-      "error",
-      {
-        "ts-expect-error": "allow-with-description",
-        "ts-ignore": "allow-with-description",
-        "ts-nocheck": "allow-with-description",
-        "ts-check": "allow-with-description",
-        minimumDescriptionLength: 5,
-      },
-    ],
-    "jest/no-deprecated-functions": 0,
-  },
+    root: true,
+    parser: "@typescript-eslint/parser",
+    extends: ["preact", "plugin:@typescript-eslint/recommended", "prettier"],
+    ignorePatterns: ["dist/"],
+    rules: {
+        "@typescript-eslint/ban-ts-comment": [
+            "error",
+            {
+                "ts-expect-error": "allow-with-description",
+                "ts-ignore": "allow-with-description",
+                "ts-nocheck": "allow-with-description",
+                "ts-check": "allow-with-description",
+                minimumDescriptionLength: 5,
+            },
+        ],
+        "jest/no-deprecated-functions": 0,
+    },
 };
